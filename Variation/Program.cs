57ad52624508aa3274ca3cdcc7c8c56f03c42d8d
victorjouin <davidmoresta1@gmail.com>
2022-03-29 15:51:16 +0200

    project variation log work

--- Variation/Program.cs
+++ Variation/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Variation
 {
@@ -6,7 +11,68 @@ namespace Variation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MySqlCommand sqlCommand;
+            string[] cripty(int[] pawd, int[] id)
+            {
+                string str = "Sed condimentum dictum ex a vestibulum.Duis ullamcorper ante mollis ipsum aliquam, in elementum lectus fringilla.Sed" +
+                    "blandit elementum magna, id ornare elit. Suspendisse viverra pretium scelerisque. Phasellus ultricies consequat gravida. In Convallis" +
+                    "vestibulum malesuada. Praesent molestie dolor eros, ac volutpat ante aliquaM in. Etiam ac est facilisis erat viverra interdum.Suspendisse" +
+                    "a tortor eleifend, dictum urna eu, tempor ex.Donec pellentesque felis sed turpis fringilla, in rutrum est fringilla.Vestibulum ornare " +
+                    "risus sed leo ornare, non pulvinar odio fermentum.Phasellus pellentesque imperdiet " +
+                    "tellus, ut molestie diam ultrices sit amet.Nunc augue leo, volutpat nec suscipit et, maximus in lectus.Nam mollis vehicula interdum." +
+                    "Quisque in ante lacus. Nullam eget porta augue. Phasellus et tincidunt nisi." +
+                    "Sed cursus magna id nisi fringil5la, et dapibus turpis consectet7ur.Curabitur porttitor elit vitae velit malesuada" +
+                    "porttitor.Proin efficitur lect6us a venenat4is laor31eet. Vestibu6lum nec magna tell5us. Nunc maximus sapien et ante pulvinar," +
+                    "ut tincidunt sapien tincid7unt.Fusce vitae8 orci sed q9uam iacu2lis volutp01at quis 4vitae enim.";
+
+                char[] listknow = new char[pawd.Length];
+                char[] listword = new char[id.Length];
+
+                int i = 0;
+
+                foreach (int x in pawd)
+                {
+                    listknow[i] = str[x];
+                    i++;
+                }
+
+                int j = 0;
+
+                foreach (int y in id)
+                {
+                    listword[j] = str[y];
+                    j++;
+                }
+
+                string[] result = new string[2];
+                result[0] = string.Join("", listknow);
+                result[1] = string.Join("", listword);
+
+                return result;
+
+
+            }
+
+            int[] know = { 233, 48, 186 };
+            int[] word = { 469, 69, 694, 869, 969, 1114, 1125, 1114, 1114 };
+            string[] list = cripty(know, word);
+            string user = list[0];
+            string pwd = list[1];
+            Console.WriteLine(user);
+            Console.WriteLine(pwd);
+
+            string connetionString = "server=78.123.62.241;uid=" + user + ";password=" + pwd + ";database=ppe;";
+            try
+            {
+                MySqlConnection cnn = new MySqlConnection(connetionString);
+                cnn.Open();
+                sqlCommand = cnn.CreateCommand();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message, "Erreur de connexion");
+            }
+
         }
     }
 }
